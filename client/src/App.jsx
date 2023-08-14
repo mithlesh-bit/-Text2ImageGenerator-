72dfc714ccbb7fc1690c5e15b93ff88f4f83f6e5
mithlesh-bit <mithleshrawte6@gmail.com>
2023-08-14 21:21:04 +0530

    final

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,21 +3,32 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import { logo } from './assets'
 import { Home, CreatePost } from './pages'
 
+
+
 export const App = () => {
   return (
     <BrowserRouter>
-      <header className='w-full flex justify-between itme-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+      <header className='w-full flex justify-between itme-center bg-[#FFC600] sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
         <Link to="/">
-          <img src={ logo } alt="logo" className='w-28 object-contain' />
+          <img src={ logo } alt="logo" className='w-48 object-contain' />
         </Link>
-        <Link to="/create-post" className='font-inter font-medium bg-purple-500 text-white px-4 py-4 rounded-md'>Create</Link>
+        <Link to="/create-post" className='font-inter h-12 font-medium bg-black text-white px-4 py-3 rounded-md'>CREATE</Link>
       </header>
-      <main className='sm:p-8 px-4 py-8 w-full bg-gray-100 min-h-[calc(100vh-78px)]'>
+      <main className='sm:p-8 px-4 py-8 w-full bg-white min-h-[calc(100vh-78px)]'>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create-post" element={<CreatePost />} />
       </Routes>
       </main>
+      <footer className='w-full bg-[#FFC600] py-4 text-center text-gray-800'>
+  <div className='flex flex-col items-center space-y-2'>
+    <p className='text-xl text-white  underline '>Mithlesh Rawte</p>
+    <a href="https://mith-dot.onrender.com/">click(primarySite)</a>
+    <p className='text-xs'>Email: rawtemithlesh52@gamil.com</p>
+    <p className='text-xs'>Phone: (+91) 9770366745</p>
+    <p className='text-xs'>Bilaspur Chhattisgarh India</p>
+  </div>
+</footer>
     </BrowserRouter>
   )
 }
--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -3,15 +3,15 @@ import React from 'react'
  const FormField = ({LableName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe}) => {
   return (
     <div>
-      <div className=' flex items-center gap-2 mb-2'>
+      <div className='flex items-center gap-2 mb-2'>
         <label htmlFor={name} className='block text-sm font-medium text-gray-900'>
           {LableName}
         </label>
         {isSurpriseMe && (
-          <button type='button' onClick={handleSurpriseMe} className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'>Surprise me</button>
+          <button type='button' onClick={handleSurpriseMe} className='font-semibold text-xs bg-[#FFC600] py-1 px-2 border-black border-2 rounded-[5px] text-white'>RANDOM</button>
         )}
       </div>
-      <input type={type} id={name} name={name} placeholder={placeholder}  value={value} onChange={handleChange} required className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'      />
+      <input type={type} id={name} name={name} placeholder={placeholder}  value={value} onChange={handleChange} required className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#FFC600] focus:border-[#FFC600] outline-none block w-full p-3' />
     </div>
   )
 }
